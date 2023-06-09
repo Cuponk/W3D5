b487bf9c001499ff270c220bd8da8f14d72f9187
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-09 15:09:13 -0700

    add knights travailis

---
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -0,0 +1,10 @@
+require_relative 'practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb'
+
+class KnightPathFinder
+    def initialize(start)
+        @self.root_node = PolyTreeNode.new(start)
+        self.build_move_tree
+    end
+
+    
+end
