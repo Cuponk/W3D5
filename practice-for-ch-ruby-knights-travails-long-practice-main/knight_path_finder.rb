347775467fb19c222aeec832c68f3eefed91f7dc
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-09 17:35:40 -0700

    Add build_move_tree

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -1,17 +1,21 @@
-require_relative 'practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb'
+require_relative '../practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb'
 
+require 'byebug'
 class KnightPathFinder
+
+    attr_reader :considered_positions, :root_node
+
     def initialize(start)
-        @self.root_node = PolyTreeNode.new(start)
-        self.build_move_tree
+        @root_node = PolyTreeNode.new(start)
         @considered_positions = [start]
+        self.build_move_tree
     end
     MOVE_DIRECTIONS = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
 	def self.valid_moves(pos)
         moves = []
         MOVE_DIRECTIONS.each do |direction|
             new_positions = [direction[0] + pos[0], direction[1] + pos[1]]
-            if (0..7).include?(new_positions[0]) && (0..7).include?(new_move_positions[1])
+            if (0..7).include?(new_positions[0]) && (0..7).include?(new_positions[1])
                 moves << new_positions
             end
         end
@@ -19,19 +23,29 @@ class KnightPathFinder
 	end
 
 	def new_move_positions(pos)
-        res = []
-        KnightPathFinder.valid_moves(pos).reject { |i| considered_positions.include?(i) ? res << i : @considered_positions << i }
-        res
+        new_positions = KnightPathFinder.valid_moves(pos).reject { |i| @considered_positions.include?(i) } #new_positions
+        @considered_positions.concat(new_positions)
+        new_positions
 	end
 
 	def build_move_tree
 		queue = []
-		queue << @self.root_node
+		queue << @root_node
         while !queue.empty?
             temp = queue.shift
-            new_move_positions(temp).each do |ele|
-                queue << ele if !@considered_positions.include?(ele)
+            new_move_positions(temp.value).each do |ele|
+                new_child = PolyTreeNode.new(ele)
+                temp.add_child(new_child)
+                queue << new_child
             end
         end
 	end
 end
+
+test = KnightPathFinder.new([0, 0])
+
+# p KnightPathFinder.valid_moves([4, 3])
+# p test.new_move_positions([4, 3])
+# p test.new_move_positions([2, 2])
+p test.root_node
+
